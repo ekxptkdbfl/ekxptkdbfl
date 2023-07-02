f5b6925cf22134b77908d573c5385f0bf09f6b33
ekxptkdbfl <133082582+ekxptkdbfl@users.noreply.github.com>
2023-07-03 01:36:41 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ function filter() {
     let col = document.getElementsByClassName("col");
 
     for (let i = 0; i < col.length; i++) {
-        tagu = col[i].getElementsByClassName("tagu");
         kadonaiyo = col[i].getElementsByClassName("kadonaiyo");
-        if (tagu[0].innerHTML.toLowerCase().indexOf(search) !== -1 ||
-            kadonaiyo[0].innerHTML.toLowerCase().indexOf(search) !== -1
+        if (kadonaiyo[0].innerHTML.toLowerCase().indexOf(search) !== -1
         ) {
             col[i].style.display = "flex"
         } else {
